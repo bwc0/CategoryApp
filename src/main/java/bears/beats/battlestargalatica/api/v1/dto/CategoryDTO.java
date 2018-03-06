41a188cd197bad3c89c8d4c08904f89ee2f16b65
bwc0 <bryantcason32@gmail.com>
2018-03-06 11:16:04 -0500

    adding project

---
+++ src/main/java/bears/beats/battlestargalatica/api/v1/dto/CategoryDTO.java
@@ -0,0 +1,6 @@
+package bears.beats.battlestargalatica.api.v1.dto;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/bears/beats/battlestargalatica/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package bears.beats.battlestargalatica.repository;
+
+import bears.beats.battlestargalatica.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+}
